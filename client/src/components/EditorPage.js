070e218c7f192ec991377338fb5cb759857b4772
Ashubegum-11 <ashubegum548@gmail.com>
2025-06-09 15:59:18 +0530

    Update EditorPage.js

--- client/src/components/EditorPage.js
+++ client/src/components/EditorPage.js
@@ -114,10 +114,10 @@ function EditorPage() {
   const runCode = async () => {
     setIsCompiling(true);
     try {
-      const response = await axios.post("http://localhost:5000/compile", {
-        code: codeRef.current,
-        language: selectedLanguage,
-      });
+      const response = await axios.post("https://collabide-rzjw.onrender.com/compile", {
+  code: codeRef.current,
+  language: selectedLanguage,
+});
       console.log("Backend response:", response.data);
       setOutput(response.data.output || JSON.stringify(response.data));
     } catch (error) {
